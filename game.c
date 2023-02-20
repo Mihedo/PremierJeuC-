e672fb03c43c52530076c665e519d93dedc3700a
Mihedo <123761016+Mihedo@users.noreply.github.com>
2023-02-20 16:25:11 +0100

    Create game.c

---
+++ game.c
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "function.hpp"
+
+int main()
+{
+    game();
+    gameasy();
+    gamenormal();
+    gamehard();
+    gamextreme();
+
+    return 0;
+}
